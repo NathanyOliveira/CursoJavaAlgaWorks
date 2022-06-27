c8f877f00fd87f181caf8afdedf668b8429eea7b
Nathany <naty_om_3@hotmail.com>
2022-06-26 22:01:01 -0300

    Adding Chapter 9 - POO

---
+++ Capitulo09/src/Exercicio01/Main.java
@@ -0,0 +1,20 @@
+package Exercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Produto produto = new Produto();
+		produto.nome = "Notebook XZ10";
+		produto.quantidadeEstoque = 8;
+		
+		System.out.println("Necessário repor estoque do produto " + produto.nome + "? " + eNecessarioReporEstoque(produto));
+	}
+	
+	static Boolean eNecessarioReporEstoque(Produto produto) {
+		if (produto.quantidadeEstoque < Produto.QUANTIDADE_MINIMA_ESTOQUE) {
+			return true;
+		}
+		
+		return false;
+	}
+}
